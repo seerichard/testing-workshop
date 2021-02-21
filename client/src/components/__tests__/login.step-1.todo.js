3b575d40bfa756916466ff4811aba269376b98c6
Richard See <richard.see@argos.co.uk>
2021-02-21 23:56:03 +0000

    Test a Form Component

--- client/src/components/__tests__/login.step-1.todo.js
+++ client/src/components/__tests__/login.step-1.todo.js
@@ -1,22 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Login from '../login'
+
 // Basic unit test
 test('calls onSubmit with the username and password when submitted', () => {
   // Arrange
-  // create a fake object to hold the form field values (username and password)
   // create a jest.fn() for your submit handler
+  const handleSubmit = jest.fn();
+
   // render the Login component to a div
   // TIP: const div = document.createElement('div')
-  //
+  const container = document.createElement('div');
+  ReactDOM.render(<Login onSubmit={handleSubmit} />, container);
+
   // get the field nodes
   // TIP: const inputs = div.querySelectorAll('input')
   // TIP: const form = div.querySelector('form')
   // fill in the field values
-  //
+  const form = container.querySelector('form');
+  const { username, password } = form.elements;
+  username.value = 'chuck norris';
+  password.value = 'I do not need a password';
+
   // Act
   // submit the form:
   // TIP: formNode.dispatchEvent(new window.Event('submit'))
-  //
+  form.dispatchEvent(new window.Event('submit'));
+
   // Assert
   // ensure your submit handler was called properly
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username: username.value,
+    password: password.value
+  });
 })
 
 //////// Elaboration & Feedback /////////
